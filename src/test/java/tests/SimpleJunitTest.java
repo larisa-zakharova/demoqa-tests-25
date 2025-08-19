f0ebbeff42c1f3dd39430c485dc53b5ff59d476b
Larisa Zakharova <zakharoval78@gmail.com>
2025-08-20 01:53:56 +0300

    added CssXpathExamples

--- src/test/java/tests/SimpleJunitTest.java
+++ src/test/java/tests/SimpleJunitTest.java
@@ -7,41 +7,44 @@ public class SimpleJunitTest {
     int result;
 
     @BeforeAll
-    static  void beforeAll(){
+    static void beforeAll() {
         System.out.println("### beforeAll()\n");
 
     }
+
     @BeforeEach
-    void beforeEach(){
+    void beforeEach() {
         System.out.println("###  beforeEach()");
         result = getResult();
     }
 
     @AfterEach
-    void afterEach(){
+    void afterEach() {
         System.out.println("###  afterEach()\n");
         result = 0;
     }
 
     @AfterAll
-    static void afterAll(){
+    static void afterAll() {
         System.out.println("\n###  afterAll()\n");
     }
 
     @Test
     void firstTest() {
         System.out.println("###     firstTest()");
-        Assertions.assertTrue(result>2);
+        Assertions.assertTrue(result > 2);
     }
+
     @Test
     void secondTest() {
         System.out.println("###     secondTest()");
-        Assertions.assertTrue(result>2);
+        Assertions.assertTrue(result > 2);
     }
+
     @Test
     void thirdTest() {
         System.out.println("###     thirdTest()");
-        Assertions.assertTrue(result>2);
+        Assertions.assertTrue(result > 2);
     }
 
     public int getResult() {
--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -10,28 +10,29 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class TextBoxTests {
 
-        @BeforeAll
-        static  void beforeAll(){
-            Configuration.browserSize = "1920x1080";
-            Configuration.baseUrl = "https://demoqa.com";
-            Configuration.pageLoadStrategy = "eager";
-            Configuration.holdBrowserOpen = true;
-            Configuration.timeout = 5000; // default 4000
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        //Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000; // default 4000
 
 
     }
-        @Test
-        void fillFormTest() {
-            open("/text-box");
-            $("#userName").setValue("Alex");
-            $("#userEmail").setValue("alex@egorov.com");
-            $("#currentAddress").setValue("Some street1");
-            $("#permanentAddress").setValue("Another street 1");
-            $("#submit").click();
 
-            $("#output #name").shouldHave(text("Alex"));
-            $("#output #email").shouldHave(text("alex@egorov.com"));
-            $("#output #currentAddress").shouldHave(text("Some street1"));
-            $("#output #permanentAddress").shouldHave(text("Another street 1"));
-        }
+    @Test
+    void fillFormTest() {
+        open("/text-box");
+        $("#userName").setValue("Alex");
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#currentAddress").setValue("Some street1");
+        $("#permanentAddress").setValue("Another street 1");
+        $("#submit").click();
+
+        $("#output #name").shouldHave(text("Alex"));
+        $("#output #email").shouldHave(text("alex@egorov.com"));
+        $("#output #currentAddress").shouldHave(text("Some street1"));
+        $("#output #permanentAddress").shouldHave(text("Another street 1"));
+    }
 }
